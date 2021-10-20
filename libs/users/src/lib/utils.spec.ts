65d673ffe56c443a4343e14d6c156e750a4d279c
Luca Pasquale <lucapasquale.094@gmail.com>
2021-10-20 13:54:25 -0300

    checkpoint

--- libs/users/src/lib/utils.spec.ts
+++ libs/users/src/lib/utils.spec.ts
@@ -1,6 +1,10 @@
 import { isLuca } from './utils';
 
 describe('isLuca', () => {
+  it('should return false if no user', () => {
+    expect(isLuca(null)).toEqual(false);
+  });
+
   it('should work', () => {
     expect(isLuca({ id: '1', name: 'Luca' })).toEqual(true);
     expect(isLuca({ id: '1', name: 'Lucas' })).toEqual(false);
